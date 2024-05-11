21524986cb75a32f5365fd4dda05abb8c30e091c
1434947721 <1434947721abc@gmail.com>
2024-05-11 13:56:32 +0800

    Update

---
+++ Assets/TutorialInfo/Scripts/FireSystem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FireSystem : MonoBehaviour
+{
+    [SerializeField, Header("buill")]
+    private GameObject prefabBullet;
+    [SerializeField, Header("buill_place")]
+    private Transform firepoint;
+
+}
