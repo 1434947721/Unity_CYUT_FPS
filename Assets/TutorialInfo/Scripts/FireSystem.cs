8437624026d5f1be38f1e73f8d8fc314d1a78c02
1434947721 <1434947721abc@gmail.com>
2024-05-11 14:51:54 +0800

    Update

--- Assets/TutorialInfo/Scripts/FireSystem.cs
+++ Assets/TutorialInfo/Scripts/FireSystem.cs
@@ -6,5 +6,17 @@ public class FireSystem : MonoBehaviour
     private GameObject prefabBullet;
     [SerializeField, Header("buill_place")]
     private Transform firepoint;
+    [SerializeField, Header("buill_speed"), Range(0, 3000)]
+    private float firespeed = 500;
 
+    private void Awake()
+    {
+        FireBullet();
+    }
+
+    private void FireBullet()
+    {
+        GameObject temp = Instantiate(prefabBullet, firepoint.position, Quaternion.identity);
+        temp.GetComponent<Rigidbody>().AddForce(transform.forward * firespeed);
+    }
 }
