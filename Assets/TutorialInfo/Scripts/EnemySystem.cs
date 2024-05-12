33c0812f3664cb37a95737b47d240a5af1b1cde7
1434947721 <1434947721abc@gmail.com>
2024-05-12 11:30:37 +0800

    Update

--- Assets/TutorialInfo/Scripts/EnemySystem.cs
+++ Assets/TutorialInfo/Scripts/EnemySystem.cs
@@ -7,9 +7,21 @@ public class ESystem : MonoBehaviour
     private Transform playerPoint;
     [SerializeField, Header("DH")]
     private NavMeshAgent agent;
+    [SerializeField, Header("anime")]
+    private Animator ani;
+
+    private string parMove = "Move_nb";
+    private string parAttack = "New Trigger";
 
     private void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         agent.SetDestination(playerPoint.position);
+
+        ani.SetFloat(parMove, agent.velocity.magnitude / agent.speed);
     }
 }
