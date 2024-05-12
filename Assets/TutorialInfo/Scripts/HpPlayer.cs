1b23bd81d929d1ee8d31ff567a96f6121f692026
1434947721 <1434947721abc@gmail.com>
2024-05-12 16:09:28 +0800

    Update

--- Assets/TutorialInfo/Scripts/HpPlayer.cs
+++ Assets/TutorialInfo/Scripts/HpPlayer.cs
@@ -1,8 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HpPlayer : HpSystem
 {
+    [SerializeField, Header("HPfount")]
+    private Image imgHP;
+    [SerializeField, Header("Text")]
+    private TMP_Text textHp;
+    
     private string enemyAttackArea = "atk_area";
+    private float hpMax;
+
+    private void Awake()
+    {
+        hpMax = hp;
+        UpdateUI();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,5 +26,16 @@ public class HpPlayer : HpSystem
         }
     }
 
+    private void UpdateUI()
+    {
+        imgHP.fillAmount = hpMax / hpMax;
+        textHp.text = $"HP: {hp} / {hpMax}";
+    }
 
+    protected override void Damage(float damage)
+    {
+        if (hp <= 0) return;
+        base.Damage(damage);
+        UpdateUI();
+    }
 }
